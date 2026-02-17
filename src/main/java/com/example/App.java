56f9ff726dd7827d4229a647cde3707b7f86d740
prathuedu-create <prathuedu@gmail.com>
2026-02-17 21:21:19 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,16 +4,4 @@ public class App {
  return a + b;
  }
 }
-COMMIT THE CHANGES
 
-FOR THE AppTest.JAVA use the FOLLOWING CODE:
-package com.example;
-import org.junit.Test;
-import static org.junit.Assert.*;
-public class AppTest {
- @Test
- public void testAdd() {
- App app = new App();
- assertEquals(5, app.add(2, 3));
- }
-}
