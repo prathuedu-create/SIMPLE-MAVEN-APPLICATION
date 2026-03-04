ea46c80988636cd7297084a1e332014399b5e4c0
Prathusha S <prathuedu@gmail.com>
2026-03-04 15:56:47 +0530

    Changes made

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,27 +1,20 @@
 package com.example;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 public class AppTest {
-
     App calc = new App();
-
     @Test
     public void testAdd() {
         assertEquals(15, calc.add(10, 5));
     }
-
     @Test
     public void testSubtract() {
         assertEquals(5, calc.subtract(10, 5));
     }
-
     @Test
     public void testMultiply() {
         assertEquals(50, calc.multiply(10, 5));
     }
-
     @Test
     public void testDivide() {
         assertEquals(2, calc.divide(10, 5));
